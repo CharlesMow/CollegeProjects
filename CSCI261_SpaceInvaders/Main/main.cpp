7b70fb9a538911331880155b071852460f3112d8
CharlesMowbray <charlesmowbray@mines.edu>
2022-12-16 13:50:06 -0700

    first commit

---
+++ CSCI261_SpaceInvaders/Main/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//#include <SFML/Graphics.hpp>
+//using namespace sf;
+
+
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
